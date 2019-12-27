b93a39a5c1d1a8a726c9663f65055d2695b281d1
behnaz-motavali <bs.motavali@gmail.com>
2019-12-27 10:50:31 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Welcome to my web store</h1>
+            </header>
+        </div>
+        )
+    }
+}
